9d10843c28a04783e9e666acbf424cfaa72fd673
Nicolas Doualot <public@slubman.info>
2010-01-23 06:53:12 +0100

    Russian translation end, from Alexander Potashev

--- l10n/screenie_ru.ts
+++ l10n/screenie_ru.ts
@@ -1,11 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
 <context>
     <name>HelpForm</name>
     <message>
         <location filename="../help.ui" line="13"/>
         <source>Instructions</source>
-        <translation type="unfinished"></translation>
+        <translation>Руководство пользователя</translation>
     </message>
     <message>
         <location filename="../help.ui" line="19"/>
@@ -18,7 +19,15 @@ p, li { white-space: pre-wrap; }
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Drag and drop an image&lt;/span&gt; from file manager or web browser to change the image. You can change the left, center and right image individually.&lt;/p&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;/p&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Right-click&lt;/span&gt; to save the result to a file (in PNG format).&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&amp;quot;&amp;gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Используйте ползунки&lt;/span&gt; в окне &lt;span style=&quot; font-style:italic;&quot;&gt;Параметры&lt;/span&gt; для изменения внешнего вида.&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;При &lt;span style=&quot; font-weight:600;&quot;&gt;перетаскивании&lt;/span&gt; изображения из диспетчера файлов или веб-браузера будет меняться одно из трёх изображений. Левое, центральное и правое изображения меняются отдельно.&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;Нажмите &lt;span style=&quot; font-weight:600;&quot;&gt;правую кнопку мыши&lt;/span&gt; для сохранение результата в формате PNG.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
@@ -26,74 +35,74 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../parameters.ui" line="19"/>
         <source>Parameters</source>
-        <translation type="unfinished"></translation>
+        <translation>Параметры</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="31"/>
         <source>Left Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Левое изображение</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="276"/>
         <source>Distance</source>
-        <translation type="unfinished"></translation>
+        <translation>Расстояние</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="293"/>
         <source>Angle</source>
-        <translation type="unfinished"></translation>
+        <translation>Поворот</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="140"/>
         <source>Center Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Центральное изображение</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="239"/>
         <source>Right Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Правое изображение</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="352"/>
         <source>Reflection</source>
-        <translation type="unfinished"></translation>
+        <translation>Отражение</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="366"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Яркость</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="414"/>
         <source>Background</source>
-        <translation type="unfinished"></translation>
+        <translation>Фон</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="426"/>
         <source>R</source>
-        <translation type="unfinished"></translation>
+        <translation>R</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="456"/>
         <source>G</source>
-        <translation type="unfinished"></translation>
+        <translation>G</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="486"/>
         <source>B</source>
-        <translation type="unfinished"></translation>
+        <translation>B</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="253"/>
         <source>Offset</source>
         <comment>Image offset</comment>
-        <translation type="unfinished"></translation>
+        <translation>Смещение</translation>
     </message>
     <message>
         <location filename="../parameters.ui" line="383"/>
         <source>Offset</source>
         <comment>Opacity offset</comment>
-        <translation type="unfinished"></translation>
+        <translation>Высота</translation>
     </message>
 </context>
 <context>
@@ -101,7 +110,7 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../screenie.cpp" line="416"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Сохранение</translation>
     </message>
 </context>
 </TS>
